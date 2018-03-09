e2f36e83d95b033042537552f2503577850f3dce
Gavin <gavin.fitzgerald@ucdconnect.ie>
2018-03-09 13:23:42 +0000

    Added regular expressions for on off and switch.

--- led_tester/led_tester.py
+++ led_tester/led_tester.py
@@ -11,6 +11,10 @@ class LEDTester:
         self.lights = [[False]*N for _ in range(N)]
         
     def apply(self, led_cmd):
-        pass
+        
+        on_pat = re.compile(".*(turn on)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+        off_pat = re.compile(".*(turn off)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+        switch_pat = re.compile(".*(switch)\s*([+-]?\d+)\s*,\s*([+-]?\d+)\s*through\s*([+-]?\d+)\s*,\s*([+-]?\d+).*")
+        
     def count(self):
         pass

1f0ec71d0a0678dc33140847339963ccad6b7a3d
Gavin <gavin.fitzgerald@ucdconnect.ie>
2018-03-09 12:29:23 +0000

    CLI test.

--- tests/test_assignment3_led_tester.py
+++ tests/test_assignment3_led_tester.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Tests for `assignment3_led_tester` package."""
+"""Tests for `led_tester` package."""
+
+import sys
+sys.path.append('.')
 
 import pytest
 
@@ -12,27 +15,9 @@ from assignment3_led_tester import cli
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
-
-
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
 
 def test_command_line_interface():
     """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'assignment3_led_tester.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    ifile = "./data/test_data.txt"
+    N, instructions = utils.parseFile(ifile)
+    assert N is not None
